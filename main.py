50e188a5ca16f5693a0413c803544149c8c1b000
Astra42 <enshov@mail.ru>
2023-02-25 20:40:48 +0500

    created first WSGI-app

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+#перове WSGI приложение
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+@app.route('/index')
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/about')
+def about():
+    return '<h1> опс а тут будет о сайте</h1>'
+
+@app.route('/g')
+def g():
+    return '<h1> g</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)#запуск локального сервера
+
+
+
+
+
